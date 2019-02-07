dc0a234a096814b59893f038e6cea7aac1e99fe6
zariuz <nechetov@hotmail.com>
2019-02-07 23:03:40 +0300

    page layout dialogs

--- src/components/Dialogs/Dialogs.js
+++ src/components/Dialogs/Dialogs.js
@@ -3,8 +3,29 @@ import s from './Dialogs.module.css';
 
 const Dialogs = () => {
   return (
-    <div>
-      Hello!
+    <div className="dialogs">
+      <div className="dialogs-items">
+        <div className="dialog">
+          Evgeniy
+        </div>
+        <div className="dialog">
+          Jorick
+        </div>
+        <div className="dialog">
+          Olya
+        </div>
+        <div className="dialog">
+          Lena
+        </div>
+        <div className="dialog">
+          Alecksandr
+        </div>
+      </div>
+      <div className="messages">
+        <div className="message">Hi</div>
+        <div className="message">Hello</div>
+        <div className="message">I'm fine</div>
+      </div>
     </div>
   );
 }
