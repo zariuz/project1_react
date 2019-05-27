064391d9ece3770dd7729422515bb26525d46e91
zariuz <nechetov@hotmail.com>
2019-05-27 23:14:45 +0300

    profile edit

--- src/components/Profile/MyPosts/MyPosts.js
+++ src/components/Profile/MyPosts/MyPosts.js
@@ -1,28 +1,26 @@
-import React from 'react';
-import p from './MyPosts.module.css';
-import Post from './Post/Post';
+import React from "react";
+import s from "./MyPosts.module.css";
+import Post from "./Post/Post";
 
-const MyPosts = (props) => {
-  
-  let postsElements = props.posts
-    .map ( p => <Post message={p.message} likesCount={p.likesCount}/>);
+const MyPosts = props => {
+  let postsElements = props.posts.map(p => (
+    <Post message={p.message} likesCount={p.likesCount} />
+  ));
 
   return (
-    <div className={p.postsBlock}>
+    <div className={s.postsBlock}>
       <h3>My posts</h3>
       <div>
         <div>
-          <textarea></textarea>
+          <textarea />
         </div>
         <div>
           <button>Add post</button>
         </div>
       </div>
-      <div className={p.posts}>
-        {postsElements}
-      </div>
+      <div className={s.posts}>{postsElements}</div>
     </div>
   );
-}
+};
 
 export default MyPosts;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
 let posts = [
-  {id: 1, message: "Hi Yo", likesCount: 15},
-  {id: 2, message: "Hello", likesCount: 25},
-]
+  { id: 1, message: "Hi Yo", likesCount: 15 },
+  { id: 2, message: "Hello", likesCount: 25 },
+  { id: 3, message: "Welcome", likesCount: 5 }
+];
 
-ReactDOM.render(<App posts={posts}/>, document.getElementById('root'));
+ReactDOM.render(<App posts={posts} />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
